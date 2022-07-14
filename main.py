5059b9118012448158c90651ec254516ee35b6be
Yasin Kaya <yasinkaya.121@gmail.com>
2022-07-14 16:56:31 +0300

    refactor(auth): Refactor authentication

--- main.py
+++ main.py
@@ -3,13 +3,9 @@ Main code file.
 """
 
 import json
-from core.auth import Auth
 from core.modem import Modem
 
 config = {}
-auth = Auth(config)
-
-auth.load_certificates()
 
 HOST = "a2q4ztq1aigmmt-ats.iot.us-west-2.amazonaws.com"
 PORT = 8883
@@ -18,8 +14,7 @@ PAYLOAD_JSON = {"state": {"reported": {"Status": "Test message from Picocell!"}}
 payload = json.dumps(PAYLOAD_JSON)
 
 
-modem = Modem()
+modem = Modem(config)
 
 if __name__ == "__main__":
     print("Starting...")
-
