8fd0bd2d9cde20626df00fa91caee7aceb5cfa87
Gökhan Koçmarlı <gokhankocmarli@gmail.com>
2022-08-24 18:57:30 +0300

    refactor: Telegram example is simplified

--- examples/telegram_example_with_manager.py
+++ examples/telegram_example_with_manager.py
@@ -1,15 +1,15 @@
 """
 Example code for sending message to group chat in Telegram with using its Bot API.
 """
-
 import time
+
 from core.modem import Modem
 from core.temp import debug
 
 modem = Modem()
 
 while True:
-    result = modem.telegram.send_message("This_is_an_example!")
+    result = modem.telegram.send_message("Picocell Telegram Example")
     debug.info(result)
 
     time.sleep(10)
