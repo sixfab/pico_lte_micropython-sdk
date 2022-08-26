020fcd02475a7af21de869ec7267886483af2979
Yasin Kaya <yasinkaya.121@gmail.com>
2022-08-27 00:24:47 +0300

    Add feature to save last response message to return the end of day

--- core/temp.py
+++ core/temp.py
@@ -9,6 +9,7 @@ class StateCache:
     """Data class for storing state data"""
 
     states = {}
+    last_response = None
 
     def add_cache(self, function_name):
         """Gets cache for #function_name or adds new cache with #function_name key"""
@@ -21,6 +22,14 @@ class StateCache:
     def set_state(self, function_name, state):
         """Sets state of function_name"""
         self.states[function_name] = state
+
+    def get_last_response(self):
+        """Returns last response"""
+        return self.last_response
+
+    def set_last_response(self, response):
+        """Sets last response"""
+        self.last_response = response
 
 config = {}
 cache = StateCache()
