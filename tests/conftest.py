add69678b6676b06ee092eff323431e44c423009
Gökhan Koçmarlı <gokhankocmarli@gmail.com>
2022-10-25 15:23:52 +0200

    feat: Add tests for `atcom.py` file (#54)
    * refactor: PEP-8 rules & better remove file method
    
    * feat: Add tests for ATCom class
    
    * feat: Update mocking usage according to standarts

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,41 @@
 import os
+import errno
 import pytest
 
 
 def write_file(file_path, data, file_type="t"):
+    """This function creates a file with data.
+    """
     try:
-        with open(file_path, "w" + file_type) as file:
+        with open(file_path, "w" + file_type, encoding="utf-8") as file:
             file.write(data)
-    except:
+    except OSError:
         return None
     else:
         return data
 
+def remove_file(file_path):
+    """This function deletes a file in the file-system if exists.
+    """
+    try:
+        os.remove(file_path)
+    except OSError as error:
+        if error.errno != errno.ENOENT:
+            raise error
 
-mock_machine = """
+
+MOCK_MACHINE_PY = """
 class UART:
     def __init__(self, *args, **kwargs):
         pass
 
     def write(self):
+        pass
+
+    def any(self):
+        pass
+
+    def read(self):
         pass
 
 
@@ -35,35 +53,38 @@ class I2C:
         pass
 """
 
-mock_neopixel = """
+MOCK_NEOPIXEL_PY = """
 class NeoPixel:
     
     def __init__(self, *args, **kwargs):
         pass
 """
 
-mock_ubinascii = ""
+MOCK_UBINASCII_PY = ""
 
 
 def prepare_test_enviroment():
     """
     Function for preparing test enviroment
     """
-
-    write_file("machine.py", mock_machine)
-    write_file("neopixel.py", mock_neopixel)
-    write_file("ubinascii.py", mock_ubinascii)
+    write_file("machine.py", MOCK_MACHINE_PY)
+    write_file("neopixel.py", MOCK_NEOPIXEL_PY)
+    write_file("ubinascii.py", MOCK_UBINASCII_PY)
 
 
 @pytest.hookimpl()
 def pytest_sessionstart(session):
+    """This method auto-runs each time tests are started.
+    """
     print("Test enviroment preparing...")
     prepare_test_enviroment()
 
 
 @pytest.hookimpl()
 def pytest_sessionfinish(session):
+    """This method auto-runs each time tests are ended.
+    """
     print("Test enviroment cleaning...")
-    os.remove("machine.py")
-    os.remove("neopixel.py")
-    os.remove("ubinascii.py")
+    remove_file("machine.py")
+    remove_file("neopixel.py")
+    remove_file("ubinascii.py")
