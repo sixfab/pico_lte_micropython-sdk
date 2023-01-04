1d1ef330c16acfb59818b547ed15d385c431cdb0
Gökhan Koçmarlı <gokhankocmarli@gmail.com>
2023-01-04 17:09:13 +0300

    feat: Create the main controller class for Crux board

---
+++ core/crux.py
@@ -0,0 +1,27 @@
+"""This module contains the Crux class which represents the functionality of the Sixfab Crux board."""
+
+from core.modules.ulp import ULP
+from core.modules.peripherals import Periph
+from core.modules.modem import Modem
+
+from core.apps.aws import AWS
+from core.apps.slack import Slack
+from core.apps.telegram import Telegram
+from core.apps.thingspeak import ThingSpeak
+from core.apps.azure import Azure
+from core.apps.scriptr import Scriptr
+
+class Crux:
+    """This class represents the functionality of the Sixfab Crux board."""
+    def __init__(self):
+        self.ulp = ULP()
+        self.peripherals = Periph()
+        self.modem = Modem()
+        self.wifi = None
+
+        self.aws = AWS(self.modem, self.wifi)
+        self.telegram = Telegram(self.modem, self.wifi)
+        self.thingspeak = ThingSpeak(self.modem, self.wifi)
+        self.slack = Slack(self.modem, self.wifi)
+        self.azure = Azure(self.modem, self.wifi)
+        self.scriptr = Scriptr(self.modem, self.wifi)
