8bc036aeef549f345264f0d259ffaf4d028d117c
Gökhan Koçmarlı <gokhankocmarli@gmail.com>
2023-01-04 18:42:44 +0300

    refactor: Fix typos and add BOTH

--- core/utils/enums.py
+++ core/utils/enums.py
@@ -1,13 +1,16 @@
-"""Enumarations for the Crux SDK"""
+"""Enumerations for the Crux SDK"""
 
 
 class Status:
+    """Enumeration for Status responses."""
     SUCCESS = 0
     ERROR = 1
     TIMEOUT = 2
     ONGOING = 3
     UNKNOWN = 99
 
-class Conection():
+class Connection:
+    """Enumeration for Connection responses."""
     CELLULAR = 0
     WIFI = 1
+    BOTH = 2
