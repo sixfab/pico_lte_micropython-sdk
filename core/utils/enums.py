f74cb3d04922b09dc6b50979d63add0bc00fd735
Gökhan Koçmarlı <gokhankocmarli@gmail.com>
2023-01-04 21:13:15 +0300

    feat: Add enum types for WiFi manager

--- core/utils/enums.py
+++ core/utils/enums.py
@@ -10,7 +10,24 @@ class Status:
     UNKNOWN = 99
 
 class Connection:
-    """Enumeration for Connection responses."""
+    """Enumeration for connection types."""
     CELLULAR = 0
     WIFI = 1
     BOTH = 2
+
+class WiFiSecurity:
+    """Enumeration for WiFi security levels types."""
+    OPEN = 0
+    WEP = 1
+    WPA_PSK = 2
+    WPA2_PSK = 3
+    WPA_WPA2_PSK = 4
+
+class WiFiStatus:
+    """Enumeration for status responses of WiFi driver."""  
+    IDLE = 0
+    CONNECTING = 1
+    WRONG_PASSWORD = 2
+    NO_AP_FOUND = 3
+    CONNECTION_FAILED = 4
+    GOT_IP = 5
