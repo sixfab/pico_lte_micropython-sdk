712db9bda7cf6f31405ef0b3be49cf80dfca3978
Gökhan Koçmarlı <gokhankocmarli@gmail.com>
2023-03-31 16:14:22 +0300

    refactor: Change the name of product

--- main.py
+++ main.py
@@ -2,17 +2,17 @@
 Main code file.
 """
 
-from core.crux import Crux
+from core.sixfab_lte import SixfabLTE
 from core.temp import debug
 
-crux = Crux()
+devboard = SixfabLTE()
 
 
 def main():
     debug.info("Hello from Sixfab...")
-    debug.info("Your Picocell is ready to use!")
+    debug.info("Your Sixfab LTE Development Board is ready to use!")
     debug.info("Visit the following link to get started")
-    debug.info("http://docs.sixfab.com/picocell/")
+    debug.info("http://docs.sixfab.com/sixfab_lte_dev_board/")
 
 
 if __name__ == "__main__":
