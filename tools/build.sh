1a535fba1062456e473a8b94442cd1179d15d5ea
Gökhan Koçmarlı <gokhankocmarli@gmail.com>
2023-04-01 18:17:54 +0300

    feat: Add tools to automatically build and upload

---
+++ tools/build.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+# This script is used to embed the library as a frozen module and build the firmware.
+
+DOWNLOAD_LOC="/tmp"
+
+# Internal variables
+PROJECT_DIR=$(pwd)
+BUILD_ID="picoLTE-$(date +%Y-%m-%d-%H-%M-%S)"
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NOCOLOR='\033[0m'
+
+print_the_status_of_command() {
+    if [ $? -eq 0 ]; then
+        echo -e " ${GREEN}OK${NOCOLOR}"
+    else
+        echo -e " ${RED}FAILED${NOCOLOR}"
+        exit 1
+    fi
+}
+
+download_firmware() {
+    cd $DOWNLOAD_LOC
+    # Check if the MicroPython source code is already downloaded
+    if [ -d "micropython" ]; then
+        echo -n "- MicroPython source code already downloaded. Pulling the latest changes..."
+        cd micropython
+        git pull > /dev/null
+    else
+        echo -n "- Downloading latest MicroPython source code..."
+        git clone git@github.com:micropython/micropython.git > /dev/null
+    fi
+
+    print_the_status_of_command
+}
+
+find_os_of_the_user() {
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        echo "- Linux OS detected."
+        OS="linux"
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "- MacOS detected."
+        OS="macos"
+    else
+        echo "- Unknown OS detected. Exiting!"
+        exit 1
+    fi
+}
+
+download_the_toolchain_macos() {
+    echo -n "- Downloading the toolset for MacOS..."
+    # Download CMake using brew
+    brew install cmake > /dev/null 2>&1
+    # Download the toolchain using brew
+    brew tap ArmMbed/homebrew-formulae > /dev/null 2>&1
+    brew install arm-none-eabi-gcc > /dev/null 2>&1
+
+    print_the_status_of_command
+}
+
+download_the_toolchain_linux() {
+    echo -n "- Downloading the toolset for Linux with apt-get..."
+    # Download CMake
+    sudo apt-get install cmake > /dev/null 2>&1
+    # Download the toolchain
+    sudo apt-get install gcc-arm-none-eabi > /dev/null 2>&1
+
+    print_the_status_of_command
+}
+
+prepare_the_environment() {
+    find_os_of_the_user
+    if [ "$OS" == "macos" ]; then
+        download_the_toolchain_macos
+    elif [ "$OS" == "linux" ]; then
+        download_the_toolchain_linux
+    fi
+
+    cd $DOWNLOAD_LOC/micropython
+
+    echo -n "- Building the MicroPython cross-compiler..."
+    make -C mpy-cross > /dev/null
+    print_the_status_of_command
+
+    cd ports/rp2
+
+    echo -n "- Building the git submodules..."
+    make BOARD=PICO_W submodules > /dev/null
+    print_the_status_of_command
+
+    echo -n "- Cleaning the older build files..."
+    make BOARD=PICO_W clean > /dev/null
+    print_the_status_of_command
+}
+
+copy_the_library_as_frozen_module() {
+    echo -n "- Copying the library to frozen modules..."
+    # Delete the older version of the library.
+    rm -rf $DOWNLOAD_LOC/micropython/ports/rp2/modules/core > /dev/null 2>&1
+    cp -r $PROJECT_DIR/core $DOWNLOAD_LOC/micropython/ports/rp2/modules/
+    print_the_status_of_command
+}
+
+build_the_firmware() {
+    echo -n "- Building the firmware with frozen module..."
+    make BOARD=PICO_W > /dev/null 2>&1
+    print_the_status_of_command
+    
+    # Copy the firmware to the project directory.
+    mkdir -p $PROJECT_DIR/build
+    echo -n "- Copying the firmware to the project directory..."
+    cp build-PICO_W/firmware.uf2 $PROJECT_DIR/build/$BUILD_ID.uf2
+    print_the_status_of_command
+
+    echo "- Firmware build ID: $BUILD_ID"
+}
+
+wait_user_to_connect_the_pico() {
+    # Display the prompts in green
+    echo -e "\n${GREEN}- Please connect the PicoLTE board to your computer while pressing BOOTSEL button.${NOCOLOR}"
+    echo -e "${GREEN}- Press any key to continue...${NOCOLOR}\n"
+
+    # Wait for the user to press a key
+    read -n 1 -s
+}
+
+upload_the_firmware() {
+    echo -n "- Uploading the firmware to the board..."
+    cd $DOWNLOAD_LOC/micropython/ports/rp2
+
+    if [ "$OS" == "macos" ]; then
+        cp build-PICO_W/firmware.uf2 /Volumes/RPI-RP2
+    elif [ "$OS" == "linux" ]; then
+        cp build-PICO_W/firmware.uf2 /media/$USER/RPI-RP2
+    fi
+
+    print_the_status_of_command
+}
+
+# Main Function
+download_firmware
+prepare_the_environment
+copy_the_library_as_frozen_module
+build_the_firmware
+echo "- Building process completed."
---
+++ tools/upload.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+# This script is used to upload the firmware to the Pico board.
+
+# Internal variables
+FW_LOCATIONS=$(pwd)/build
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NOCOLOR='\033[0m'
+
+print_the_status_of_command() {
+    if [ $? -eq 0 ]; then
+        echo -e " ${GREEN}OK${NOCOLOR}"
+    else
+        echo -e " ${RED}FAILED${NOCOLOR}"
+        exit 1
+    fi
+}
+
+
+find_os_of_the_user() {
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        OS="linux"
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        OS="macos"
+    else
+        echo -e "${RED}- ERROR: Unknown OS detected. Exiting!${NOCOLOR}"
+        exit 1
+    fi
+}
+
+wait_user_to_connect_the_pico() {
+    # Display the prompts in green
+    echo -e "${GREEN}- Please connect the PicoLTE board to your computer while pressing BOOTSEL button.${NOCOLOR}"
+    echo -e "${GREEN}- Press any key to continue...${NOCOLOR}"
+
+    # Wait for the user to press a key
+    read -n 1 -s
+}
+
+upload_the_firmware() {
+    echo -n -e "- Uploading the firmware ${BUILD_ID} to the board..."
+
+    if [ "$OS" == "macos" ]; then
+        # Check if the volume exists.
+        if [ ! -d "/Volumes/RPI-RP2" ]; then
+            echo -e " ${RED}ERROR: The PicoLTE is not connected. Exiting!${NOCOLOR}"
+            exit 1
+        else
+            cp $FW_LOCATIONS/$BUILD_ID.uf2 /Volumes/RPI-RP2
+        fi
+    elif [ "$OS" == "linux" ]; then
+        # Check if the volume exists.
+        if [ ! -d "/media/$USER/RPI-RP2" ]; then
+            echo -e "${RED}ERROR: The PicoLTE is not connected. Exiting!${NOCOLOR}"
+            exit 1
+        else
+            cp $FW_LOCATIONS/$BUILD_ID.uf2 /media/$USER/RPI-RP2
+        fi
+    fi
+
+    print_the_status_of_command
+}
+
+argument_parser() {
+    HELP_TEXT="Usage: $0 <firmware_build_id>\nExample: $0 picoLTE-2023-01-01-01"
+
+    if [ -z "$1" ]; then
+        echo -e $HELP_TEXT
+        exit 1
+    elif [ "$1" == "--help" ]; then
+        echo -e $HELP_TEXT
+        exit 0
+    elif [ "$1" == "-h" ]; then
+        echo -e $HELP_TEXT
+        exit 0
+    else
+        BUILD_ID="$1"
+    fi
+}
+
+# Main function
+argument_parser $1
+wait_user_to_connect_the_pico
+find_os_of_the_user
+upload_the_firmware
+echo "- Uploading process completed."
