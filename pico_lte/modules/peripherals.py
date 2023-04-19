98458e78fa7334e36d79c1804ee04c54d681c20b
Gökhan Koçmarlı <gokhankocmarli@gmail.com>
2023-04-19 18:18:38 +0300

    feat: Change pins and remove battery

--- pico_lte/modules/peripherals.py
+++ pico_lte/modules/peripherals.py
@@ -2,7 +2,7 @@
 Module for incuding periheral hardware functions of picocell module.
 """
 
-from machine import Pin, I2C, ADC
+from machine import Pin, I2C
 from neopixel import NeoPixel
 
 
@@ -10,15 +10,12 @@ class Periph:
     """
     Class for inculding periheral hardware functions of picocell module.
     """
+    user_button = Pin(21, Pin.IN)
+    user_led = Pin(22, Pin.OUT)
+    pico_led = Pin("LED", Pin.OUT)
+    neopixel = Pin(15, Pin.OUT)
 
-    battery_voltage_pin = Pin(29, Pin.IN)
-    battery_charge_status_pin = Pin(25, Pin.IN)
-    user_button_pin = Pin(2, Pin.IN)
-    neopixel_pin = Pin(10, Pin.OUT)
-
-    qwiic_sda_pin = Pin(6)
-    qwiic_scl_pin = Pin(7)
-    qwiic = I2C(1, scl=qwiic_scl_pin, sda=qwiic_sda_pin, freq=400_000)
+    qwiic = I2C(0, scl=Pin(17), sda=Pin(16), freq=400_000)
 
     def __init__(self):
         """
@@ -34,7 +31,7 @@ class Periph:
         status : int
             User button status
         """
-        return self.user_button_pin.value()
+        return self.user_button.value()
 
     def adjust_neopixel(self, red, green, blue):
         """
@@ -49,34 +46,9 @@ class Periph:
         blue : int
             Blue color value (0-255)
         """
-        neopixel = NeoPixel(self.neopixel_pin, 8)
+        neopixel = NeoPixel(self.neopixel, 8)
         neopixel[0] = (red, green, blue)
         neopixel.write()
-
-    def get_battery_voltage(self):
-        """
-        Function for getting battery voltage
-
-        Returns
-        -------
-        voltage : float
-            Battery voltage
-        """
-        adc_module = ADC(self.battery_voltage_pin)
-        raw_16_bit = adc_module.read_u16()
-        value_in_volts = (raw_16_bit / 65535) * 3.3
-        return value_in_volts
-
-    def get_charge_status(self):
-        """
-        Function for getting charge status
-
-        Returns
-        -------
-        status : int
-            Charge status
-        """
-        return self.battery_charge_status_pin.value()
 
     def qwiic_scan(self):
         """
