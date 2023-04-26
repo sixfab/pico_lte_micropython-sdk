c12850cae139d1c4e3c0c44c2b791ef72b469f4e
Gökhan Koçmarlı <gokhankocmarli@gmail.com>
2023-04-26 14:37:17 +0300

    docs: update Scriptr example after the fixes

--- examples/scriptr/post_json.py
+++ examples/scriptr/post_json.py
@@ -6,20 +6,20 @@ config.json file must include the following parameters for this example:
 config.json
 {
     "scriptr":{
-        "query": "[QUERY_OF_SCRIPT]",
-        "authorization": "[YOUR_TOKEN]"
+        "endpoint": "[API_ENDPOINT_QUERY]",
+        "device_token": "[DEVICE_TOKEN]"
     }
 }
+Example endpoint query for the "Hello" scriptr in Scriptr.io is "HelloDevice".
+You can create the device token on the Device Directory page in Scriptr.io.
 """
 import json
 from pico_lte.core import PicoLTE
 from pico_lte.common import debug
 
 picoLTE = PicoLTE()
-
-payload_json = {"temp": "25"}
-payload = json.dumps(payload_json)
+payload = json.dumps({"temp": "25"})
 
 debug.info("Sending data to Scriptr.io script...")
-result = picoLTE.scriptr.send_data(payload)
+result = picoLTE.scriptr.post_json(payload)
 debug.info("Result:", result)
