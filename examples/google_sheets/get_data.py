ac07053f8d922602daf62c0de42a937c03557d74
Ozgur Karaaslan <ozgurkaraaslan29@gmail.com>
2023-04-27 00:51:14 +0300

    docs: update comment lines for new config parameters

--- examples/google_sheets/get_data.py
+++ examples/google_sheets/get_data.py
@@ -1,20 +1,30 @@
 """
+Example code for getting data from a Google Sheets document with using its API.
+
+Example Configuration
+---------------------
+Create a config.json file in the root directory of the PicoLTE device.
+config.json file must include the following parameters for this example:
+
 config.json
 {
     "google_sheets":{
-        "api_key":"[API_KEY]",
-        "spreadsheetId": "[spreadsheetId]",
-        "majorDimension": [majorDimension],
-        "valueRenderOption": [valueRenderOption],
-        "dateTimeRenderOption": [dateTimeRenderOption]
+        "host": "[YOUR_HOST_URL]",
+        "api_key": "[API_KEY_OF_GOOGLE_SHEETS_DOCUMENT]",
+        "token": "[AUTHORIZATION_TOKEN]",
+        "majorDimension": "[MAJOR_DIMENSION_OPTION]",
+        "valueRenderOption": "[VALUE_RENDER_OPTION]",
+        "data_range": [DATA_RANGE_OF_TABLE],
+        "sheet": [SHEET_NAME]
     }
 }
+
 """
 from pico_lte.core import PicoLTE
 from pico_lte.common import debug
 
 picoLTE = PicoLTE()
 
-debug.info("Creating a google sheet...")
+debug.info("Getting data from the google sheet...")
 result = picoLTE.google_sheets.get_data()
 debug.info("Result:", result)
