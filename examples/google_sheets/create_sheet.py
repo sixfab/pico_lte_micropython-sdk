553eedf5472f5b5afe4d522f410aa06faec6f00d
Ozgur Karaaslan <ozgurkaraaslan29@gmail.com>
2023-05-08 02:54:53 +0300

    feat: create_sheet method usage example and documentation

---
+++ examples/google_sheets/create_sheet.py
@@ -0,0 +1,25 @@
+"""
+Example code for creating new spreadsheet and sheets with using its API.
+
+Example Configuration
+---------------------
+Create a config.json file in the root directory of the PicoLTE device.
+config.json file must include the following parameters for this example:
+
+config.json
+{
+    "google_sheets":{
+        "api_key": "[API_KEY_OF_GOOGLE_SHEETS_DOCUMENT]",
+        "OAuthToken": "[AUTHORIZATION_TOKEN]",
+        "spreadsheetId": "[SPREAD_SHEET_ID]"
+    }
+}
+"""
+from pico_lte.core import PicoLTE
+from pico_lte.common import debug
+
+picoLTE = PicoLTE()
+debug.set_level(0)
+debug.info("Posting data to the google sheet...")
+result = picoLTE.google_sheets.create_sheet(sheets=["SHEET1", "SHEET2"])
+debug.info("Result:", result)
