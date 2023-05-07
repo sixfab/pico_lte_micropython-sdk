c418fae9295c5dce3a52509fa418e728bd45300a
Ozgur Karaaslan <ozgurkaraaslan29@gmail.com>
2023-05-08 02:48:54 +0300

    docs: update docstring for the new config format

--- examples/google_sheets/post_data.py
+++ examples/google_sheets/post_data.py
@@ -1,5 +1,5 @@
 """
-Example code for posting data to a Google Sheets document with using its API.
+Example code for posting data to a Google Sheets table with using its API.
 
 Example Configuration
 ---------------------
@@ -9,24 +9,20 @@ config.json file must include the following parameters for this example:
 config.json
 {
     "google_sheets":{
-        "host": "[YOUR_HOST_URL]",
         "api_key": "[API_KEY_OF_GOOGLE_SHEETS_DOCUMENT]",
-        "token": "[AUTHORIZATION_TOKEN]",
-        "spreadsheetId": "[SPREAD_SHEET_ID]",
-        "post": {
-            "data_range": "[DATA_RANGE_OF_TABLE]",
-            "valueInputOption": "[VALUE_RENDER_OPTION]",
-            "majorDimension": "[MAJOR_DIMENSION_OPTION]",
-            "sheet": "[SHEET_NAME]"
-        }
+        "OAuthToken": "[AUTHORIZATION_TOKEN]",
+        "spreadsheetId": "[SPREAD_SHEET_ID]"
     }
 }
+
 """
 from pico_lte.core import PicoLTE
 from pico_lte.common import debug
 
 picoLTE = PicoLTE()
 
-debug.info("Posting data to the google sheet...")
-result = picoLTE.google_sheets.post_data(data=[[7, 8, 9]], data_range="A5:C5")
+debug.info("Posting data to the Google Sheets table...")
+result = picoLTE.google_sheets.post_data(
+    data=[[1, 2, 3], [4, 5, 6]], data_range="A1:C2"
+)
 debug.info("Result:", result)
