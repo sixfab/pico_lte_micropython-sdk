039751a7cc95e0b653a48156a70a9578a90ecebb
Ozgur Karaaslan <ozgurkaraaslan29@gmail.com>
2023-08-04 20:14:08 +0300

    chore: user example arrangements

--- examples/google_sheets/add_data.py
+++ examples/google_sheets/add_data.py
@@ -20,11 +20,11 @@ config.json
 """
 from pico_lte.core import PicoLTE
 from pico_lte.common import debug
-debug.set_level(0)
+
 picoLTE = PicoLTE()
 
 debug.info("Adding data to the Google Sheets table...")
 result = picoLTE.google_sheets.add_data(
-    sheet="Sayfa1", data=[[1, 2, 3], [4, 5, 6]], data_range="A1:C2"
+    sheet="Sheet11", data=[[1, 2, 3], [4, 5, 6]], data_range="A1:C2"
 )
 debug.info("Result:", result)
