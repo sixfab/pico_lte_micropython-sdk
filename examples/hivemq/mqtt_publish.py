7ad394d2f0b1abb14ef2174ee1c12ea31f20817d
Resul Ozdemir <zemirsd9@gmail.com>
2024-03-18 16:40:53 +0200

    feat : Added hivemq.py, mqtt_publish.py and mqtt_subscribe.py

---
+++ examples/hivemq/mqtt_publish.py
@@ -0,0 +1,32 @@
+"""
+Example code for publising data to HiveMQ by using MQTT.
+
+Example Configuration
+---------------------
+Create a config.json file in the root directory of the PicoLTE device.
+config.json file must include the following parameters for this example:
+
+config.json
+{
+    "hivemq": {
+        "mqtts":{
+            "url":"[YOUR_URL]",
+            "port": [PORT_NUMBER],
+            "username": "[HIVEMQ_USERNAME]",
+            "password": "[HIVEMQ_PASSWORD]",
+            "pub_topic": "[YOUR_PUBLISH_TOPIC]"
+         }
+    }
+}
+"""
+
+from pico_lte.core import PicoLTE
+from pico_lte.common import debug
+
+picoLTE = PicoLTE()
+
+payload = "Hello HiveMQ! This is a test message."
+
+debug.info("Publishing data to HiveMQ...")
+result = picoLTE.hivemq.publish_message(payload)
+debug.info("Result", result)
