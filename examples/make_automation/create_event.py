1883eab958f73c0b5fa60bfd2e1045dbb6a6c191
Akif Artun <akifartn@gmail.com>
2024-03-24 16:18:22 +0300

    feat: make_automation examples

---
+++ examples/make_automation/create_event.py
@@ -0,0 +1,31 @@
+"""
+Example code for creating event in Google Calendar with using Make Automation.
+
+Example Configuration
+---------------------
+Create a config.json file in the root directory of the PicoLTE device.
+config.json file must include the following parameters for this example:
+config.json
+{
+    "make_automation":{
+        "url": "[INCOMING_WEBHOOK_URL]",
+    }
+}
+"""
+
+import json
+from pico_lte.core import PicoLTE
+from pico_lte.common import debug
+
+debug.set_level(0)
+
+picoLTE = PicoLTE()
+
+payload_json = {"Name": "Meeting with someone.",
+                "Start_Date": "27.03.2024 09:15",
+                "Duration": "01:30"}
+payload = json.dumps(payload_json)
+
+debug.info("Setting event on Google Calendar.")
+result = picoLTE.make_automation.send_data(payload)
+debug.info("Result: ", result)
---
+++ examples/make_automation/get_weather.py
@@ -0,0 +1,29 @@
+"""
+Example code to send weather forecast to Telegram using Make Automation.
+
+Example Configuration
+---------------------
+Create a config.json file in the root directory of the PicoLTE device.
+config.json file must include the following parameters for this example:
+config.json
+{
+    "make_automation":{
+        "url": "[INCOMING_WEBHOOK_URL]",
+    }
+}
+"""
+
+import json
+from pico_lte.core import PicoLTE
+from pico_lte.common import debug
+
+debug.set_level(0)
+
+picoLTE = PicoLTE()
+
+payload_json = {"City": "Adana"}
+payload = json.dumps(payload_json)
+
+debug.info("The weather forecast for tomorrow is being sent to Telegram.")
+result = picoLTE.make_automation.send_data(payload)
+debug.info("Result: ", result)
